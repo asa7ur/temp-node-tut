7c2fb932ca8410cea3230532c7701a671093c88d
Garik Asatryan <gar.asat.96@gmail.com>
2023-10-16 19:03:38 +0200

    first commit

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
-/* 
-npm - global command, comes with node
-npm --version
-
-local dependency - use it only in this particular project
-npm i <packageName>
-
-global dependency - use it in any project
-npm install -g <packageName> 
-
-package.json - manifest file (stores important info about project/package)
-manual approach (create package.json in the root, create properties etc)
-npm init (step by step, press enter to skip)
-npm init -y (everything default) 
-*/
-
-const _ = require("lodash")
-
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items);
-console.log(newItems)
-console.log("hello people")
+// started operating system process
+console.log('first')
+setTimeout(() => {
+  console.log('second')
+}, 0)
+console.log('third')
+// completed and exited operating system process
