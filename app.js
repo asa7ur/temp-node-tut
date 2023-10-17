d15499283fa3c38335382090ab5c32761f9c84f7
Garik Asatryan <gar.asat.96@gmail.com>
2023-10-17 16:03:51 +0200

    added async await files

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-// started operating system process
-console.log('first')
-setTimeout(() => {
-  console.log('second')
-}, 0)
-console.log('third')
-// completed and exited operating system process
+const {readFile, writeFile} = require("fs").promises
+/* const util = require("util");
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile) */
+
+const start = async() => {
+  try{
+    const first = await readFile("./content/first.txt", "utf8")
+    const second = await readFile("./content/second.txt", "utf8")
+    await writeFile("./content/result-mind-grenade.txt", 
+    `THIS IS AWESOME!!!!: ${first} ${second}`)
+    console.log(first,second)
+  } catch(error){
+    console.log(error)
+  } 
+}
+
+start()
+
+/* 
+const getText = (path) => {
+  return new Promise((resolve, reject)=>{
+    readFile(path, "utf8", (err,data)=>{
+      if(err){
+        reject(err)
+      }
+      else{
+        resolve(data)
+      }
+    })
+  })
+}
+
+getText("./content/first.txt")
+  .then(result => console.log(result))
+  .catch((err) => console.log(err)) 
+*/
