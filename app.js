d1f53ffd059b3a0e54f7bf4969947ac62254504f
Garik Asatryan <gar.asat.96@gmail.com>
2023-10-17 18:51:38 +0200

    new files

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const EventEmitter = require("events");
+const http = require('http')
 
-const customEmitter = new EventEmitter()
+// const server = http.createServer((req, res) => {
+//   res.end('Welcome')
+// })
 
-customEmitter.on("response",(name, id)=>{
-  console.log(`data received: ${name} with id: ${id}`)
+// Using Event Emitter API
+const server = http.createServer()
+// emits request event
+// subcribe to it / listen for it / respond to it
+server.on('request', (req, res) => {
+  res.end('Welcome')
 })
-customEmitter.on("response",()=>{
-  console.log(`some other logic here`)
-})
 
-
-customEmitter.emit("response","John",34)
+server.listen(5000)
